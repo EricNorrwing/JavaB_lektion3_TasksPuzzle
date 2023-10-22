ad13e088d1f1ec58d499e70ba0f3b65a324fdf54
EricNorrwing <eric.norrwing@gmail.com>
2023-10-22 17:37:23 +0200

    ITS ALIVE

--- src/com/EricNorrwing/TasksPuzzle/Main.java
+++ src/com/EricNorrwing/TasksPuzzle/Main.java
@@ -1,4 +1,33 @@
 package com.EricNorrwing.TasksPuzzle;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        calculator calc = new calculator();
+
+        System.out.println("insert the first number");
+        calc.a = scanner.nextInt();
+        System.out.println("insert the second number");
+        calc.b = scanner.nextInt();
+        System.out.println("What do you want to do ? (+ - * /");
+
+        var result = switch (scanner.next()){
+            case "*" -> calc.multiplication();
+            case "/" -> calc.division();
+            case "+" -> calc.addition();
+            case "-" -> calc.subtraction();
+            default -> throw new
+                    IllegalStateException("Unexpected value: ");
+        };
+        System.out.println(result);
+
+
+
+
+
+    }
+
+
 }
---
+++ src/com/EricNorrwing/TasksPuzzle/calculator.java
@@ -0,0 +1,21 @@
+package com.EricNorrwing.TasksPuzzle;
+
+public class calculator {
+
+    static int a;
+    static int b;
+
+    public int addition(){
+        return a+b;
+    }
+    public int subtraction(){
+        return a-b;
+    }
+    public int multiplication(){
+        return a*b;
+    }
+    public int division(){
+        return a/b;
+    }
+
+}
